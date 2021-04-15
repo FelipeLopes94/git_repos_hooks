b77d62cbfc10bb22d9a24bc0caba2b48c987666f
felipelopes94 <felipee.c.lopess@gmail.com>
2021-04-14 21:54:46 -0300

    Up do Projeto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import "./App.css"
+export default function App(){
+  const [repositories, setRepositories] = useState([])
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  useEffect(() => {
+    async function fetchData(){
+      const response = await fetch("https://api.github.com/users/felipelopes94/repos")
+      const data = await response.json();
+  
+      setRepositories(data);
+    }
+    fetchData()
+
+  }, []); //ARRAY VAZIO, VAI EXECUTAR UMA VEZ APENAS, PQ NAO DEPENDE DE VALOR NENHUM!
+  //  Effect para alterar o Tittle da Pagina
+  useEffect(() => {
+    const filtered = repositories.filter(repo => repo.favorite);
+    
+    document.title = `Voce tem ${filtered.length} Favoritos`;
+  }, [repositories]);
+
+  // Funcao para Favoritar
+  function handleFavorite(id){
+    const newRepositories = repositories.map(repo => {
+      
+      return repo.id === id ? {...repo, favorite: !repo.favorite} : repo
+
+    });
+
+    setRepositories(newRepositories);
+  }
+  
+  return(
+    <div className="container">
+      <h1>REPOSITORIOS DO GIT</h1>
+      <ul className="ul">
+        {repositories.map(repo => (
+
+          <li className="li" key={repo.id}> 
+            {repo.name} 
+            {repo.favorite && <span className="favorito">(Favorito)</span>}
+            <button className="botao" onClick={() => handleFavorite(repo.id)}>Favoritar</button>
+          </li>
+
+        ))}
+      </ul>
     </div>
-  );
-}
 
-export default App;
+  )
+}
